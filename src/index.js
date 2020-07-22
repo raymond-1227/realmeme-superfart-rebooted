77921308b137e50dcd68585d8e9dc89374f1d9e5
David Wheatley <davidwheatley03@gmail.com>
2020-07-22 19:49:19 +0100

    Add whitelist disabling

--- src/index.js
+++ src/index.js
@@ -31,9 +31,10 @@ client.on('ready', () => {
 
 client.on('message', (msg) => {
   const whitelistedChannels = Config.resources.channelIds.whitelistedChannels;
+  const WhitelistEnabled = whitelistedChannels.length > 0;
 
   // Not a DM or message in whitelisted channels
-  if (msg.channel.type !== 'dm' && !whitelistedChannels.includes(msg.channel.id)) {
+  if (msg.channel.type !== 'dm' && (!WhitelistEnabled || !whitelistedChannels.includes(msg.channel.id))) {
     return;
   }
 
